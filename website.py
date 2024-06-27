5eab059cac93ae585eb72a99577b0e8bf648faf4
Zachary Lantz <Zackary.lantz@gmail.com>
2024-06-27 14:55:00 -0500

    Update website.py

--- website.py
+++ website.py
@@ -32,7 +32,7 @@ resized_SD = Sleeping_dog.resize((400, 400))
 Sleeping_dog = Image.open("PXL_20231005_205004791~3.jpg")
 resized_SD = Sleeping_dog.resize((400, 400))
 dbl_gunz = Image.open("Webapp pic.jpg")
-resized_DG = dbl_gunz.resize((400, 450))
+resized_DG = dbl_gunz.resize((500, 575))
 
 #Header section
 with st.container ():
